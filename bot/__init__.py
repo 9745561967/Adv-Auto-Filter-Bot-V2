1a5b45156f4555e517ce659e6aa352396208b410
9745561967 <82279792+9745561967@users.noreply.github.com>
2021-10-22 14:20:07 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,9 +11,9 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-APP_ID = int(os.environ.get("APP_ID"))
+API_HASH = os.environ.get("API_HASH")) 
 
-API_HASH = os.environ.get("API_HASH")
+APP_ID = int(os.environ.get("APP_ID")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
