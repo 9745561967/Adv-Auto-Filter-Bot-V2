0a9407e00834da6d31b59162beaab259530907ef
9745561967 <82279792+9745561967@users.noreply.github.com>
2021-10-23 11:17:00 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -15,6 +15,8 @@ APP_ID = int(os.environ.get("APP_ID"))
 
 API_HASH = os.environ.get("API_HASH")
 
+AUTH_USERS = os.environ.get("AUTH_USERS") 
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 DB_URI = os.environ.get("DB_URI")
