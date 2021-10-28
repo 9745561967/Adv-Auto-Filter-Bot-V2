7629b0d17ce004a3fb9f31ea40b94b79ff177ef8
9745561967 <82279792+9745561967@users.noreply.github.com>
2021-10-28 14:57:44 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -8,6 +8,7 @@ from pyrogram.errors import ButtonDataInvalid, FloodWait
 
 from bot.database import Database # pylint: disable=import-error
 from bot.bot import Bot # pylint: disable=import-error
+from bot import MT_CHANNEL_USERNAME
 
 FIND = {}
 INVITE_LINK = {}
